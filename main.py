c036e2d3e2a25bd6826489c5b9da31a86ffeff23
ryanc <114698718+RyanAC12@users.noreply.github.com>
2025-03-15 10:16:51 -0400

    Import stats function to main

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-        return file_contents
-
-
-def count_words(filepath):
-    file_contents = get_book_text(filepath)
-    words = file_contents.split()
-    counter = 0
-    for word in words:
-        counter += 1
-    print(f"{counter} words found in the document")
-    
-
+from stats import count_words
 
 def main():
     #file_contents = get_book_text("books/frankenstein.txt")
